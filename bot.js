e4cb57255a33e7161d118816759692df1731654e
August Meyers <augustmeyers@Augusts-MacBook-Air.local>
2022-04-08 16:06:29 -0400

    Preparing for Dockerization

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const { Client, Intents } = require("discord.js");
 /*
 *	Bot created by: Meyers#6464
 *	Version: 2.0.3
-* 	
+* 	 
 *	Imports FS, djs, token, and the config
 *	From there it creates the discord client instance
 *	Gathers all loaded event listeners
